7ff2b918b0eccb2dd6364c796a76f20696b5ac16
Damilola Adegunwa <damilola.adegunwa@libmot.com>
2020-10-30 13:59:07 +0100

    ccommit

---
+++ IdeaApp.Data/DatabaseContext/IdeaAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace IdeaApp.Data.DatabaseContext
+{
+    public class IdeaAppDbContext : DbContext
+    {
+        public IdeaAppDbContext(DbContextOptions<IdeaAppDbContext> options): base(options)
+        {
+
+        }
+
+        #region DbSet
+
+        #endregion
+    }
+}
